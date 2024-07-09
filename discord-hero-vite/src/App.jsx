9a68188e6b7126e91b85725fd97343b416ccec68
Ole-web <porath.ole@gmail.com>
2024-07-09 15:06:16 +0200

    done2

--- discord-hero-vite/src/App.jsx
+++ discord-hero-vite/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import background from "./assets/discord-background.png";
+import logo from "./assets/discord-logo.png";
+import menuIcon from "./assets/menu-icon.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <header>
+        <img src={menuIcon} alt="Menu Icon" />
+        <img src={logo} alt="Discord Logo" />
+      </header>
+      <div
+        className="hero-section"
+        style={{ backgroundImage: `url(${background})` }}
+      >
+        <h1>IMAGINE A PLACE...</h1>
+        <p>...where you can belong to a school lub, a gaming group...</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
